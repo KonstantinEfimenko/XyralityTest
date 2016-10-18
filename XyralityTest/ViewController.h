d54b019cce9740f1f8b8ac9989c257190b377dc7
Konstantin Efimenko <konstantinefimenko@Konstantins-MacBook-Pro.local>
2016-10-18 21:19:01 +0300

    Initial Commit

---
+++ XyralityTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XyralityTest
+//
+//  Created by Konstantin Efimenko on 10/18/16.
+//  Copyright © 2016 Efimenko Ink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XyralityTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XyralityTest
+//
+//  Created by Konstantin Efimenko on 10/18/16.
+//  Copyright © 2016 Efimenko Ink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
